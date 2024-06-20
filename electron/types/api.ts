ae456b1cb5f0adbd0db54b6e5877174034fcae5f
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-06-20 19:58:14 +0900

    🏷️ chore: Add types for more APIs

--- electron/types/api.ts
+++ electron/types/api.ts
@@ -22,14 +22,14 @@ export interface LiveDetail {
   minFollowerMinute: number
   livePlaybackJson: string
   p2pQuality: string[]
-  channel: ChannelInfo
+  channel: MinChannelInfo
   livePollingStatusJson: string
   userAdultStatus: string | null
   chatDonationRankingExposure: boolean
   adParameter: AdParameter
 }
 
-interface ChannelInfo {
+interface MinChannelInfo {
   channelId: string
   channelName: string
   channelImageUrl: string
@@ -38,4 +38,52 @@ interface ChannelInfo {
 
 interface AdParameter {
   tag: string
+}
+
+export interface ChannelInfo {
+  code: number
+  message: string | null
+  content: {
+    channelId: string
+    channelName: string
+    channelImageUrl: string
+    verifiedMark: boolean
+    channelType: string
+    channelDescription: string
+    followerCount: number
+    openLive: boolean
+    subscriptionAvailability: boolean
+    subscriptionPaymentAvailability: {
+      iapAvailability: boolean
+      iabAvailability: boolean
+    }
+    adMonetizationAvailability: boolean
+  }
+}
+
+export interface LiveInfo {
+  code: number
+  message: string | null
+  content: {
+    liveTitle: string
+    status: string
+    concurrentUserCount: number
+    accumulateCount: number
+    paidPromotion: boolean
+    adult: boolean
+    clipActive: boolean
+    chatChannelId: string
+    tags: string[]
+    categoryType: string
+    liveCategory: string
+    liveCategoryValue: string
+    livePollingStatusJson: string
+    faultStatus: string | null
+    userAdultStatus: string
+    chatActive: boolean
+    chatAvailableGroup: string
+    chatAvailableCondition: string
+    minFollowerMinute: number
+    chatDonationRankingExposure: boolean
+  }
 }
